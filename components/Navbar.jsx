294ae9f733e0d15bec9288b723623e6abd20bdcf
Ariq Ramdhany <ariqathallah38@gmail.com>
2024-07-13 21:33:14 +0900

    added mailto

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 			<Link href="/" className=' inline-block self-center'>
 				<Image alt="3 words logo" src="/logo.svg" width={100} height={100}></Image>
 			</Link>
-			<a href="https://www.x.com/ariq_ram" className=' inline-block'>
+			<a href="mailto:ariq.ramdhany@.ittend.com" className=' inline-block'>
 				<div className=' bg-lightGreen px-4 py-2 border border-none rounded-full'>
 					Contact us
 				</div>
