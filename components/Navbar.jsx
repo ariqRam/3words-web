6a020963a8c6bc6e20ff8103c754a33238bc7666
Ariq Ramdhany <ariqathallah38@gmail.com>
2024-07-13 20:39:38 +0900

    initial setup

---
+++ components/Navbar.jsx
@@ -0,0 +1,21 @@
+// components/Navbar.js
+
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Navbar = () => {
+	return (
+		<nav className="flex p-5 items-start place-content-between">
+			<Link href="/" className=' inline-block self-center'>
+				<Image alt="3 words logo" src="/logo.svg" width={100} height={100}></Image>
+			</Link>
+			<Link href="https://www.x.com/ariq_ram" className=' inline-block'>
+				<div className=' bg-lightGreen px-4 py-2 border border-none rounded-full'>
+					Contact us
+				</div>
+			</Link>
+		</nav>
+	);
+};
+
+export default Navbar;
