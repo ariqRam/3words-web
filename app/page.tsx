fd2f116fe5d6632dfec9102b6b9c4c542efd1f50
Ariq Ramdhany <ariqathallah38@gmail.com>
2024-07-14 10:43:50 +0900

    minor ui fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,8 @@
 import Image from "next/image";
 
-export const metadata = {
-	title: '3words | Learn 3 words a day',
-	description: 'Learn 3 words a day',
-	twitter: {
-		card: 'summary_large_image',
-		creator: '@ariq_ram',
-		title: '3words | Learn 3 words a day',
-		description: 'Learn 3 words a day. Casually.',
-		images: [
-			{
-				url: "https://www.get3words.com/images/x-card.png",
-				width: 1200,
-				height: 630,
-				alt: "3 words Twitter card",
-			},
-		],
-	}
-}
-
 export default function Home() {
 	return (
-		<main className="m-2 bg-primary border-none rounded-[30px] flex place-content-between">
+		<div className="m-2 bg-primary border-none rounded-[30px] flex place-content-between">
 			<div className="w-[580px] leading-[80px] text-[86px] text-white p-7">
 				<div className="inline-block">
 					<h1 >you can get <span className="text-lightGreen">very far</span> with just
@@ -33,6 +14,6 @@ export default function Home() {
 			<div className="p-10">
 				<Image alt="iphone mockup" src="/ip-mock.png" width={270} height={959}></Image>
 			</div>
-		</main>
+		</div>
 	);
 }
